2d1ffd6fb4733f7bcacbb52022be08b374dcd4fd
ccrowe <ccrowe@unomaha.edu>
2021-05-26 09:28:16 -0700

    applying formatting

--- add_descriptions.el
+++ add_descriptions.el
@@ -1,57 +1,39 @@
 
-(defun fill-hash-table (fields)
+(defun fill-hash-table (fields) 
   "Add all fields as keys in the hash-table"
-  ; base case
-  (if (eq (length fields) 0) nil
-    (progn
-    (puthash (car fields) t final)
-    (fill-hash-table (cdr fields))
-    )
-  )
-  )
+                                        ; base case
+  (if (eq (length fields) 0) nil (progn (puthash (car fields) t final) 
+                                        (fill-hash-table (cdr fields)))))
 
-(defun contains-string (look4 contains)
+(defun contains-string (look4 contains) 
   "Returns if contains look4"
-  (let (
-        (find-index   (string-match-p (regexp-quote look4) contains))
-        )
-  (if find-index t find-index)
-  ))
+  (let ((find-index   (string-match-p (regexp-quote look4) contains))) 
+    (if find-index t find-index)))
 
-(defun find-matching-key (description keys)
+(defun find-matching-key (description keys) 
   "Finds a key that matches the description"
                                         ; ensure there are keys to use
   (if (eq (length keys) 0) nil
                                         ;base case
-  (if (contains-string (car keys) description)
-      (car keys)
-    (find-matching-key description (cdr keys))
-    )
-  )
-  )
+    (if (contains-string (car keys) description) 
+        (car keys) 
+      (find-matching-key description (cdr keys)))))
 
-(defun find-description-for-key (key descriptions)
-  (if (eq (length descriptions) 0) nil
-    (if (contains-string key (car descriptions)) (car descriptions)
-      (find-description-for-key key (cdr descriptions))
-      )
-    )
-  )
+(defun find-description-for-key (key descriptions) 
+  (if (eq (length descriptions) 0) nil (if (contains-string key (car descriptions)) 
+                                           (car descriptions) 
+                                         (find-description-for-key key (cdr descriptions)))))
 
 
-(defun add-hash-value (descriptions ht key)
-  (let (
-        (matching-description (find-description-for-key key descriptions))
-        )
-    (if matching-description
-        (progn (puthash key matching-description ht) (list key matching-description))
-      (list key nil))
-  ))
+(defun add-hash-value (descriptions ht key) 
+  (let ((matching-description (find-description-for-key key descriptions))) 
+    (if matching-description (progn (puthash key matching-description ht) 
+                                    (list key matching-description)) 
+      (list key nil))))
 
 
-(defun get-key-descriptions (keys descriptions ht)
-  (-map (-partial #'add-hash-value descriptions ht) keys)
-  )
+(defun get-key-descriptions (keys descriptions ht) 
+  (-map (-partial #'add-hash-value descriptions ht) keys))
 
 (setq fields-raw (f-read-text "fields.txt" 'utf-8))
 (setq fields (butlast (split-string fields-raw "\n")))
@@ -63,17 +45,11 @@
 (fill-hash-table fields)
 (setq key-descriptions (get-key-descriptions (hash-table-keys final) descriptions final))
 
-(defun print-key-values (kds)
+(defun print-key-values (kds) 
   "Print out each key and description in a csv format"
-(if (eq (length kds) 0) nil ; returns nil each time
-  (progn
-  (message "\n%s|%s\n" (nth 1 kds) (nth 0 kds))
-  (print-key-values (cdr kds))
-  )
-  )
-)
+  (if (eq (length kds) 0) nil           ; returns nil each time
+    (progn (message "%s|%s\n" (nth 0 (car kds)) 
+                    (nth 1 (car kds))) 
+           (print-key-values (cdr kds)))))
 
 (print-key-values key-descriptions)
-
-
-
--- tests.el
+++ tests.el
@@ -1,44 +1,36 @@
-(ert-deftest
+(ert-deftest 
     contains-string
     ()
   "ensure contains string returns the correct values"
-  (should (equal (contains-string "who" "whoever") t))
-  (should (equal (contains-string "who" "no") nil))
-  )
+  (should (equal (contains-string "who" "whoever") t)) 
+  (should (equal (contains-string "who" "no") nil)))
 
-(ert-deftest
+(ert-deftest 
     find-matching-key
     ()
   "should find a key that matches from the has-table"
-  (let (
-        (test-hash (make-hash-table :test #'equal))
-        (description "foo and bar")
-        (descriptions (list "foo and bar" "who"))
-        (key "foo")
-        )
-    (should (equal (find-matching-key description (hash-table-keys test-hash)) nil))
-    (puthash key t test-hash)
-    (should (equal (find-matching-key description (hash-table-keys test-hash)) "foo"))
-  ))
+  (let ((test-hash (make-hash-table :test #'equal)) 
+        (description "foo and bar") 
+        (descriptions (list "foo and bar" "who")) 
+        (key "foo")) 
+    (should (equal (find-matching-key description (hash-table-keys test-hash)) nil)) 
+    (puthash key t test-hash) 
+    (should (equal (find-matching-key description (hash-table-keys test-hash)) "foo"))))
 
 (should (equal (find-description-for-key "who" (list "who" "what")) "who"))
 (should (equal (find-description-for-key "what" (list "who" "what")) "what"))
 
 
 (setq test-hash (make-hash-table :test #'equal))
-;(hash-table-values test-hash)
+                                        ;(hash-table-values test-hash)
 (setq key "foo")
 (puthash key t test-hash)
 (setq descriptions (list "foo2" "bar2"))
-;(hash-table-values test-hash)
-;(hash-table-keys test-hash)
-(should (equal (add-hash-value key descriptions test-hash) "foo2"))
+                                        ;(hash-table-values test-hash)
+                                        ;(hash-table-keys test-hash)
+                                        ;(should (equal (add-hash-value key descriptions test-hash) "foo2"))
 
-(-map (-partial #'add-hash-value descriptions test-hash) (hash-table-keys test-hash))
-(get-key-descriptions (hash-table-keys) )
-
-
-
-
-
-
+(get-key-descriptions (hash-table-keys) descriptions test-hash)
+(-map (-partial #'add-hash-value descriptions test-hash) 
+      (hash-table-keys test-hash))
+(get-key-descriptions (hash-table-keys test-hash) descriptions test-hash)
